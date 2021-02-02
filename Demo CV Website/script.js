78eae4fa1391bbdbbdd2e59955c99430345b1871
Phạm Hồ Hữu Trí <oopscreeper2006@gmail.com>
2021-02-02 11:35:20 +0700

    Update script.js

--- Demo CV Website/script.js
+++ Demo CV Website/script.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
                 nav: false
             },
             600:{
-                items: 2.S,
+                items: 2.5,
                 nav: false
             },
             1000:{
