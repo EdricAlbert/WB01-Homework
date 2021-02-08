7705112ac0145fd898707e92c23e68ce1be399a5
Phạm Hồ Hữu Trí <oopscreeper2006@gmail.com>
2021-02-08 20:20:10 +0700

    tada

--- Demo CV Website/script.js
+++ Demo CV Website/script.js
@@ -27,3 +27,15 @@ $(document).ready(function(){
         }
     });
 });
+var modal = document.getElementById('id01');
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+var modal1 = document.getElementById('id02');
+window.onclick = function(event) {
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
+}
