bc667548d4bc9583dc9975abbd89497ec3b99e2a
Phạm Hồ Hữu Trí <oopscreeper2006@gmail.com>
2021-03-18 20:06:47 +0700

    .

--- Demo CV Website/js/script.js
+++ Demo CV Website/js/script.js
@@ -43,7 +43,7 @@ document.getElementById("click").onclick = function() {
 
 } 
 
-var modal1 = document.getElementById('id02');
+var modal1 = document.getElementById('id02');   
 window.onclick = function(event) {
     if (event.target == modal1) {
         modal1.style.display = "none";
